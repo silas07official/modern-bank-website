4d951ab3ee72c58deb08c2f4b904ae36d28e410e
silas martins <silas07official@gmail.com>
2022-09-26 19:43:41 +0100

    changed method to platform

--- modern_bank_app/src/components/Hero.jsx
+++ modern_bank_app/src/components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => (
         </div>
       </div>
       <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
-        Payment Method.
+        Payment Platform.
       </h1>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
         Our team of experts uses a methodology to identify the credit cards most
